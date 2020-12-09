ef04aad04dceaea5fab48f8869c2f154b577bccf
haha <390519764@qq.com>
2020-12-09 23:17:37 +0800

    test: 为todo下util函数增加测试用例

--- test/unit/menus/todo/util.test.ts
+++ test/unit/menus/todo/util.test.ts
@@ -1,14 +1,34 @@
 import $ from '../../../../src/utils/dom-core'
-import { getNewNode } from '../../../../src/menus/todo/util'
+import { isTodo, getNewNode } from '../../../../src/menus/todo/util'
+
+test('test isTodo', () => {
+    const todo = $(
+        `<ul style="margin:0 0 0 20px;position:relative;"><li style="list-style:none;"><span style="position: absolute;left: -18px;top: 2px;" contenteditable="false"><input type="checkbox" style="margin-right:3px;"></span>1223</li></ul>`
+    )
+    expect(isTodo(todo)).toEqual(true)
+})
+
+test('测试文本节点为空的情况', () => {
+    const testNode = $(
+        `<b><i><span>test132</span><span>232333</span></i><span>12345678</span><span>xxd</span>dddd</b>`
+    ).getNode()
+    const txt = new Text()
+    const res = getNewNode(testNode, txt, 3) as Element
+    expect(res.outerHTML).toEqual(
+        `<b><i><span>test132</span><span>232333</span></i><span>12345678</span><span>xxd</span>dddd</b>`
+    )
+})
 
 test('测试光标位置在文本节点中间生成截断新节点', () => {
     const testNode = $(
         `<b><i><span>test132</span><span>232333</span></i><span>12345678</span><span>xxd</span>dddd</b>`
     ).getNode()
-    const txt = $(testNode).childNodes()?.getNode(1).childNodes[0] as Node
-    // txt 12345678
+    const txt = $(testNode).childNodes()?.get(0).childNodes()?.getNode(1).childNodes[0] as Node
+    // txt 232333
     const res = getNewNode(testNode, txt, 3) as Element
-    expect(res.outerHTML).toEqual(`<b><span>45678</span><span>xxd</span>dddd</b>`)
+    expect(res.outerHTML).toEqual(
+        `<b><i><span>333</span></i><span>12345678</span><span>xxd</span>dddd</b>`
+    )
 })
 
 test('测试光标位置在文本节点最前面生成截断新节点', () => {
